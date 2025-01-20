0143f9604f16782285d3160c68d7e261c8891a36
osakitakuya <noilfo3hcramosa@gamil.com>
2025-01-20 21:38:38 +0900

    formを部分テンプレーに変更した

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,48 +1,51 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update]
+  
     def index
-        @articles = Article.all
+      @articles = Article.all
     end
-
+  
     def show
-        @article = Article.find(params[:id])
     end
-
+  
     def new
-        @article = Article.new
+      @article = Article.new
     end
-
     def create
-        @article = Article.new(article_params)
-        if @article.save
-             redirect_to article_path(@article), notice: '保存できたよ'
-        else
-            flash.now[:error] = '保存に失敗しました'
-            render :new
-        end
+      @article = Article.new(article_params)
+      if @article.save
+        redirect_to article_path(@article), notice: '保存できたよ'
+      else
+        flash.now[:error] = '保存に失敗しました'
+        render :new
+      end
     end
-
+  
     def edit
         @article = Article.find(params[:id])
     end
-
+  
     def update
-        @article = Article.find(params[:id])
-        if @article.update(article_params)
-            redirect_to article_path(@article), notice: '更新できました'
-        else
-            flash.now[:error] = '更新できませんでした'
-            render :edit
-        end
+      if @article.update(article_params)
+        redirect_to article_path(@article), notice: '更新できました'
+      else
+        flash.now[:error] = '更新できませんでした'
+        render :edit
+      end
     end
-
     def destroy
-        article = Article.find(params[:id])
-        article.destroy!
-        redirect_to root_path, notice: '削除に成功しました'
+      article = Article.find(params[:id])
+      article.destroy!
+      redirect_to root_path, notice: '削除に成功しました'
     end
 
     private
     def article_params
-        params.require(:article).permit(:title, :content)
+      params.require(:article).permit(:title, :content)
     end
-end
+  
+    def set_article
+      @article = Article.find(params[:id])
+    end
+  end
+  
